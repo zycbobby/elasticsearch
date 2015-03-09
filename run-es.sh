2b9ef377b524ce9828620f8a9b21c4289eb9eef4
zycbobby <zuoyuchu@gmail.com>
2015-03-09 10:41:33 +0800

    Fix run script

--- run-es.sh
+++ run-es.sh
@@ -1,1 +1,2 @@
-docker run -d -p 9200:9200 -p 9300:9300 -v ./data:/data dockerfile/elasticsearch /elasticsearch/bin/elasticsearch -Des.config=/data/elasticsearch.yml
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+docker run -d -p 9200:9200 -p 9300:9300 -v ${DIR}/data:/data es:1.4.4 /elasticsearch/bin/elasticsearch -Des.config=/data/elasticsearch.yml
